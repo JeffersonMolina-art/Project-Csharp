0650ffad48721168b4b98d403f8da566962c2dc6
JeffersonMolina-art <84147110+JeffersonMolina-art@users.noreply.github.com>
2022-02-22 20:34:22 -0600

    Subir tarea 1

---
+++ Primera tarea/Leer teclado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LeerTeclado
+{
+    /// <summary>
+    /// Leer datos del teclado
+    /// </sumary>
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Suma de dos números");
+            Console.Write("Ingrese el número 1: ");
+            // Los datos se leen como cadena
+            string dato = Console.ReadLine();
+            //convertir a número con int.Parse
+            int n1 = int.Parse(dato);
+
+            Console.Write("Ingrese el número 2: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("El resultado es de: {0}", suma);
+            
+        }
+    }
+}
